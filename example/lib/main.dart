3381c2a0c2ac000d5ba1dd1ecb50be7b49758204
postbox-18 <postboxat18@gmail.com>
2023-06-24 15:33:10 +0530

    updated calendar event list

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,6 +33,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late DateTime currentDate;
+  /*late List<EvenList> evenList = [
+    EvenList("9:32", 0xff008A5E, "2023-06-01", ""),
+    EvenList("10:20", 0xff008A5E, "2023-06-02", ""),
+    EvenList("11:00", 0xff008A5E, "2023-06-03", ""),
+    EvenList("5:20", 0xff008A5E, "2023-06-04", ""),
+    EvenList("9:20", 0xff008A5E, "2023-06-05", ""),
+    EvenList("", 0xffB73434, "2023-06-06", "LOP"),
+    EvenList("9:20", 0xff008A5E, "2023-06-07", ""),
+    EvenList("", 0xffF1BE41, "2023-06-08", "CL"),
+    EvenList("9:20", 0xff008A5E, "2023-06-09", ""),
+    EvenList("9:20", 0xff008A5E, "2023-06-10", ""),
+    EvenList("", 0xffF1BE41, "2023-06-11", "CL"),
+    EvenList("", 0xffB73434, "2023-06-12", "LOP"),
+    EvenList("9:20", 0xff008A5E, "2023-06-13", ""),
+    EvenList("9:20", 0xff008A5E, "2023-06-14", ""),
+    EvenList("9:32", 0xff008A5E, "2023-06-15", ""),
+    EvenList("10:20", 0xff008A5E, "2023-06-16", ""),
+    EvenList("11:00", 0xff008A5E, "2023-06-17", ""),
+    EvenList("5:20", 0xff008A5E, "2023-06-18", ""),
+    EvenList("9:20", 0xff008A5E, "2023-06-19", ""),
+    EvenList("", 0xffB73434, "2023-06-20", "LOP"),
+    EvenList("9:20", 0xff008A5E, "2023-06-21", ""),
+    EvenList("9:20", 0xff008A5E, "2023-06-22", ""),
+  ];*/
 
   @override
   void initState() {
